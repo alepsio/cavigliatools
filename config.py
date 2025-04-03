64f5b20c809927d77be7a2c14c63e84ec1b1e1e5
Alessio Caviglia <alepsio.ca@gmail.com>
2025-04-04 01:11:52 +0200

    altro commit psw

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'caviglia_secret')
     DB_NAME = 'cavigliatools'
     DB_USER = 'cavigliatools_user'
-    DB_PASSWORD = 'cavigliatools_user'
+    DB_PASSWORD = 'f3acV3vj9oydrXAtQXRknJmshArSIA9W'
     DB_HOST = 'dpg-cvngo8qdbo4c73a1m93g-a.frankfurt-postgres.render.com'
     DB_PORT = '5432'
